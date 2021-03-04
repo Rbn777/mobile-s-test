3a8e4cfba2ff54fba0da5804b793c8ac3278db48
Fritsch <v.fritsch1981@gmail.com>
2021-03-04 13:46:05 +0100

    move all to eventList

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AppearanceProvider } from 'react-native-appearance';
-import Constants from 'expo-constants';
-import * as Notifications from 'expo-notifications';
-
+import { ApolloClient, InMemoryCache, gql, useQuery, ApolloProvider } from '@apollo/client';
 
 import EventList from './components/EventList';
 import CreateEvent from './components/CreateEvent';
@@ -17,118 +15,48 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
+export const client = new ApolloClient({
+  uri: 'https://localhost:7777/graphql',
+  cache: new InMemoryCache()
 });
 
-export async function schedulePushNotification() {
-  await Notifications.scheduleNotificationAsync({
-    content: {
-      title: "You've got mail! 📬",
-      body: 'Here is the notification body',
-      data: { data: 'goes here' },
-    },
-    trigger: { seconds: 2 },
-  });
-}
-
-async function registerForPushNotificationsAsync() {
-  let token;
-  if (Constants.isDevice) {
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!');
-      return;
-    }
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    console.log(token);
-  } else {
-    alert('Must use physical device for Push Notifications');
-  }
-
-  if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
-
-  return token;
-}
-
 export default function App() {
-  const [expoPushToken, setExpoPushToken] = useState('');
-  const [notification, setNotification] = useState(false);
-  const notificationListener = useRef();
-  const responseListener = useRef();
-
-  useEffect(() => {
-    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-
-    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-      setNotification(notification);
-    });
-
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-      console.log(response);
-    });
-
-    return () => {
-      Notifications.removeNotificationSubscription(notificationListener);
-      Notifications.removeNotificationSubscription(responseListener);
-    };
-  }, []);
-
   return (
-    <AppearanceProvider>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
+    <ApolloProvider client={client}>
+      <AppearanceProvider>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
 
-              if (route.name === 'Evènements') {
-                iconName = focused
-                  ? 'md-folder-open-sharp'
-                  : 'folder-outline'
-              } else if (route.name === 'Settings') {
-                iconName = focused ? 'ios-settings' : 'ios-settings-outline';
-              } else if (route.name === 'Créer évènement') {
-                iconName = focused ? 'create' : 'create-outline';
-              }
-              if (route.name === 'Evènements') {
-                <Tab.Screen name="Evènements" component={EventList} options={{ tabBarBadge: Data.length, tabBarBadgeStyle: { backgroundColor: '#FC8948', color: 'white' } }} />
-              } else {
-                <Tab.Screen name="Evènements" component={EventList} options={{ tabBarBadge: Data.length, tabBarBadgeStyle: { backgroundColor: 'white', color: '#FC8948' } }} />
-              }
-
-            // You can return any component that you like here!
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-          })}
-            tabBarOptions={{
-              activeTintColor: '#FC8948',
-              inactiveTintColor: 'gray',
-            }}
-          >
-            
-          <Tab.Screen name="Evènements" component={EventList} options={{ tabBarBadge: Data.length, tabBarBadgeStyle: { backgroundColor: '#FC8948', color: 'white' } }} />
-          <Tab.Screen name="Créer évènement" component={CreateEvent} />
-          <Tab.Screen name="Settings" component={Settings} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </AppearanceProvider>
+                if (route.name === 'Evènements') {
+                  iconName = focused
+                    ? 'md-folder-open-sharp'
+                    : 'folder-outline'
+                } else if (route.name === 'Settings') {
+                  iconName = focused ? 'ios-settings' : 'ios-settings-outline';
+                } else if (route.name === 'Créer évènement') {
+                  iconName = focused ? 'create' : 'create-outline';
+                }
+                
+              // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+            })}
+              tabBarOptions={{
+                activeTintColor: '#FC8948',
+                inactiveTintColor: 'gray',
+              }}
+            >
+              
+            <Tab.Screen name="Evènements" component={EventList} options={{ tabBarBadge: Data.length, tabBarBadgeStyle: { backgroundColor: '#FC8948', color: 'white' } }} />
+            <Tab.Screen name="Créer évènement" component={CreateEvent} />
+            <Tab.Screen name="Settings" component={Settings} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </AppearanceProvider>
+    </ApolloProvider>
   );
 }
 
--- components/EventList.js
+++ components/EventList.js
@@ -1,17 +1,121 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { StyleSheet, ScrollView, View, Image, Text, Button, Platform } from 'react-native';
-import { Appearance } from 'react-native'
+import { Appearance } from 'react-native';
+import { ApolloClient, InMemoryCache, gql, useQuery } from '@apollo/client';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
 
-import { schedulePushNotification } from '../App';
 import Data from '../Data';
 
 const osTheme = Appearance.getColorScheme();
 
+const GET_EVENTS = gql`
+  query GetEvents {
+    getEvents {
+      _id
+      title
+      date
+      hour
+      image
+    }
+  }
+`;
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+async function schedulePushNotification(theme,title,date) {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: `${theme} 📬`,
+      body: `${title} le ${date} `,
+      sound: 'suuuuu.wav', // <- for Android below 8.0
+      data: {
+        date: `${date}`
+      },
+      audioAttributes: {
+        usage: Notifications.AndroidAudioUsage.NOTIFICATION_EVENT,
+        sound: 'suuuuu.wav'
+      },
+    },
+    trigger: {
+      seconds: 2,
+      channelId: 'custom', // <- for Android 8.0+, see definition above
+    },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+    
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('custom', {
+      channelId: 'custom',
+      name: 'Suuuu',
+      sound: 'suuuuu.wav',
+      audioAttributes: {
+        usage: Notifications.AndroidAudioUsage.NOTIFICATION_EVENT,
+        sound:'suuuuu.wav'
+      },
+      importance: Notifications.AndroidImportance.MAX,
+      //vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+  return token;
+}
+
 const EventList = () => {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+  const { loading, error, data } = useQuery(GET_EVENTS);
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+      console.log("notif",notification)
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
+  
+  console.log("HELLO LA DATA", data);
+
   return (
     <ScrollView style={styles.container}>
-      {Data.map((event => (
-        <View key={event.id} style={styles.eventCard}>
+      {data.getEvents.map((event => (
+        <View key={event._id} style={styles.eventCard}>
           <Image source={{ uri: event.image }} style={styles.image} />
           <View style={styles.contentBox}>
             <Text>{event.title}</Text>
@@ -21,7 +125,7 @@ const EventList = () => {
             </View>
             <Button
               title="TEST"
-              onPress={() => schedulePushNotification()}
+              onPress={async () => await schedulePushNotification(event.theme, event.title, event.date)}
             />
           </View>
         </View>
@@ -42,8 +146,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     shadowColor: "#000",
     shadowOffset: {
-	    width: 0,
-	    height: 2,
+      width: 0,
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
